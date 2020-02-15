c1126df248bf3821cdd78bf33a1e5bfb5ff9ee7b
Vladlen Gilyazetdinov <funkylen@gmail.com>
2020-02-15 13:32:40 +0300

    Added uniq indexes to email and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User
   include Mongoid::Document
   field :email, type: String
+  index({email: 1}, {unique: true, name: 'email_unique_index'})
   field :login, type: String
+  index({login: 1}, {unique: true, name: 'login_unique_index'})
   field :password, type: String
   field :name, type: String
 end
