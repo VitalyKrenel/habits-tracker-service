4af4a0c0ba75cc12322967f86910590e762ec6de
Vladlen Gilyazetdinov <funkylen@gmail.com>
2020-03-12 17:22:21 +0300

    Add some validations

--- app/models/habit_stat.rb
+++ app/models/habit_stat.rb
@@ -1,5 +1,7 @@
 class HabitStat
   include Mongoid::Document
+  include ActiveModel::Validations
+
   field :status, type: Integer
   field :date, type: Date
   embedded_in :user
@@ -8,4 +10,9 @@ class HabitStat
   STATUS_UNDEFINED = 0
   STATUS_COMPLETE = 1
   STATUS_MISSED = 2
+
+  validates :status,
+            inclusion: {in: [STATUS_COMPLETE, STATUS_MISSED, STATUS_UNDEFINED]},
+            presence: true
+  validates :date, presence: true
 end
